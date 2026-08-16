20758a157b007d44ecf715bef827a4f8db3b5f82
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-16 14:25:08 +0800

    Refactor HeroBranding component with minor adjustments

--- src/components/sections/hero-branding.tsx
+++ src/components/sections/hero-branding.tsx
@@ -62,7 +62,7 @@ const HeroBranding = () => {
         <div className="relative">
             <h1 className="text-[22px] md:text-[28px] font-extrabold leading-tight tracking-tight flex items-center justify-center gap-x-2 flex-wrap font-poppins">
             <span className="text-[#ffffff]">Unlock</span>
-            <span className="text-[#ffffff]">Walmart</span>
+            <span className="text-[#ffc220]">Walmart</span>
             <div className="flex items-center -ml-1">
               <Gift className="w-6.0 h-6.0 text-[#ffc220] fill-transparent" />
             </div>
