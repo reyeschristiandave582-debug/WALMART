098396fc2a3b00276297b7b41fb418e4be7df0e5
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-27 01:48:59 +0800

    Update image source in AnimatedBackground component
    Updated image source for one of the images in the animated background.

--- src/components/sections/background-overlay.tsx
+++ src/components/sections/background-overlay.tsx
@@ -45,7 +45,7 @@ const AnimatedBackground = () => {
             className="absolute top-1/1 right-24 w-20 h-20 object-contain animate-float-gentle"
           />
           <Image
-            src="https://i.imgur.com/PPG7aD8.png"
+            src="https://i.imgur.com/bhkaW4V.png"
             alt=""
             width={160}
             height={160}
