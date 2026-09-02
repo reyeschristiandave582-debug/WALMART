6a15b776b7e85e2e5b91dc5da7c6002d6bbb0d60
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-09-03 04:10:19 +0800

    Update background-overlay.tsx

--- src/components/sections/background-overlay.tsx
+++ src/components/sections/background-overlay.tsx
@@ -45,7 +45,7 @@ const AnimatedBackground = () => {
             className="absolute top-1/1 right-24 w-20 h-20 object-contain animate-float-gentle"
           />
           <Image
-            src="https://i.imgur.com/bhkaW4V.png"
+            src="https://i.imgur.com/X8GOuDb.png"
             alt=""
             width={160}
             height={160}
